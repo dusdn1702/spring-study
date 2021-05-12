3f181a18d43945d45f2d9ce5b04d6ebda9f30200
y_woo :) <dusdn1702@naver.com>
2021-05-13 01:07:01 +0900

    테스트 코드 개선: beforeEach 생성

--- src/test/java/com/practice/tobi/user/UserDaoTest.java
+++ src/test/java/com/practice/tobi/user/UserDaoTest.java
@@ -7,72 +7,72 @@ import static org.hamcrest.Matchers.*;
 
 import java.sql.SQLException;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.context.config.InactiveConfigDataAccessException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class UserDaoTest {
-    @Test
-    public void addAndGet() throws SQLException, ClassNotFoundException {
+    private UserDao dao;
+
+    @BeforeEach
+    private void setUp() {
         ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
         ConnectionMaker connectionMaker = new DConnectionMaker();
 
-        UserDao userDao = context.getBean("userDao", UserDao.class);
+        this.dao = context.getBean("userDao", UserDao.class);
+    }
 
-        userDao.deleteAll();
-        assertThat(userDao.getCount(), is(0));
+    @Test
+    public void addAndGet() throws SQLException, ClassNotFoundException {
+
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
 
         User user1 = new User("hi","asdf","sleep");
         User user2 = new User("hello","zxcv","happy");
 
-        userDao.add(user1);
-        userDao.add(user2);
-        assertThat(userDao.getCount(), is(2));
+        dao.add(user1);
+        dao.add(user2);
+        assertThat(dao.getCount(), is(2));
 
-        User get1 = userDao.get(user1.getId());
+        User get1 = dao.get(user1.getId());
         assertThat(user1.getName(), is(get1.getName()));
         assertThat(user1.getPassword(), is(get1.getPassword()));
 
-        User get2 = userDao.get(user2.getId());
+        User get2 = dao.get(user2.getId());
         assertThat(user2.getName(), is(get2.getName()));
         assertThat(user2.getPassword(), is(get2.getPassword()));
     }
 
     @Test
     void count() throws SQLException, ClassNotFoundException {
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-        ConnectionMaker connectionMaker = new DConnectionMaker();
-
-        UserDao userDao = context.getBean("userDao", UserDao.class);
         User user1 = new User("hi","asdf","sleep");
         User user2 = new User("hello","zxcv","happy");
         User user3 = new User("bye","qwer","sad");
 
-        userDao.deleteAll();
-        assertThat(userDao.getCount(), is(0));
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
 
-        userDao.add(user1);
-        assertThat(userDao.getCount(), is(1));
+        dao.add(user1);
+        assertThat(dao.getCount(), is(1));
 
-        userDao.add(user2);
-        assertThat(userDao.getCount(), is(2));
+        dao.add(user2);
+        assertThat(dao.getCount(), is(2));
 
-        userDao.add(user3);
-        assertThat(userDao.getCount(), is(3));
+        dao.add(user3);
+        assertThat(dao.getCount(), is(3));
     }
 
     @Test
     void getUserFailure() throws SQLException, ClassNotFoundException {
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-        ConnectionMaker connectionMaker = new DConnectionMaker();
+        setUp();
 
-        UserDao userDao = context.getBean("userDao", UserDao.class);
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
 
-        userDao.deleteAll();
-        assertThat(userDao.getCount(), is(0));
-
-        assertThatThrownBy(()-> userDao.get("nope")).isInstanceOf(IllegalAccessError.class);
+        assertThatThrownBy(()-> dao.get("nope")).isInstanceOf(IllegalAccessError.class);
 
     }
 }
